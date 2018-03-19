01b31164828b43f9d438cad7d24c1ca0c79fb819
t-Asai <takumi.a42@gmail.com>
2018-03-20 08:11:24 +0900

    revert cnn structure to default

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -28,7 +28,7 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,
                           ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
+                          strides=[1, 1, 1, 1],
                           padding='SAME')
 
 W_conv1 = weight_variable([5, 5, 1, 32])
@@ -45,10 +45,10 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([28 * 28 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 28 * 28 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
