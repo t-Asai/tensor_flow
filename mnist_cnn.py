c991f038ed3f700b980a686375bd7d122424cec1
t-Asai <takumi.a42@gmail.com>
2018-03-22 07:21:37 +0900

    try to extract estimated value

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,5 +1,7 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import numpy as np
+
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
@@ -28,8 +30,17 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,
                           ksize=[1, 2, 2, 1],
-                          strides=[1, 1, 1, 1],
+                          strides=[1, 2, 2, 1],
                           padding='SAME')
+
+
+def toOne(array, flag):
+    obj = np.zeros((array.shape[0], 2))
+    for i, item in enumerate(array):
+        if item[flag] == 1:
+            obj[i][0] = 1
+    return obj
+
 
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
@@ -45,10 +56,10 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([28 * 28 * 64, 1024])
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 28 * 28 * 64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
@@ -67,7 +78,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(20000):
+    for i in range(2000):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(feed_dict={
@@ -77,3 +88,27 @@ with tf.Session() as sess:
 
         print('test accuracy %g' % accuracy.eval(feed_dict={
             x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    flag = 1
+    for i in range(200):
+        batch = mnist.train.next_batch(50)
+        # train_step.run(feed_dict={x: batch[0], y_: toOne(batch[1], flag), keep_prob: 0.5})
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+        if i % 1000 == 0:
+            # train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: toOne(batch[1], flag), keep_prob: 1.0})
+            train_accuracy = accuracy.eval(
+                feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print('step %d, training accuracy %g' % (i, train_accuracy))
+            # print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: toOne(mnist.test.labels, flag), keep_prob: 1.0}))
+            print('test accuracy %g' % accuracy.eval(
+                feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    print(y_conv.eval(feed_dict={x: mnist.test.images, keep_prob: 1.0}))
+    for test_x, test_y in zip(mnist.test.images, mnist.test.labels):
+        answer = sess.run(tf.argmax(y_conv, 1), feed_dict={
+                          x: [test_x], keep_prob: 1.0})
+        if test_y.argmax(0) != answer[0]:
+            print(test_y.argmax(0), answer[0])
