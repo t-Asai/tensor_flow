bd457a37e6a969026a605cdb7c861d29a33355ae
t-Asai <takumi.a42@gmail.com>
2018-03-30 06:27:00 +0900

    reshape linter and change param a little

--- main.py
+++ main.py
@@ -7,42 +7,36 @@ from methods_for_adjust_params import toOne
 def run_to_cal_accuracy(data, accuracy, train_step):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(2):
+        for i in range(20000):
             batch = data.train.next_batch(50)
-            if i % 100 == 0:
-                train_accuracy = accuracy.eval(
-                    feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            if i % 1000 == 0:
+                train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
                 print('step %d, training accuracy %g' % (i, train_accuracy))
-                print(batch)
-            train_step.run(
-                feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+                # print(batch)
+                print('test accuracy %g' % accuracy.eval(feed_dict={x: data.test.images, y_: data.test.labels, keep_prob: 1.0}))
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-            print('test accuracy %g' % accuracy.eval(
-                feed_dict={x: data.test.images, y_: data.test.labels, keep_prob: 1.0}))
 
 
 def run_to_estimate(data, accuracy, train_step):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         flag = 1
-        for i in range(2):
+        for i in range(20000):
             batch = data.train.next_batch(50)
             train_step.run(feed_dict={x: batch[0], y_: toOne(
                 batch[1], flag), keep_prob: 0.5})
             # train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
             if i % 1000 == 0:
-                train_accuracy = accuracy.eval(
-                    feed_dict={x: batch[0], y_: toOne(batch[1], flag), keep_prob: 1.0})
+                train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: toOne(batch[1], flag), keep_prob: 1.0})
                 # train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
                 print('step %d, training accuracy %g' % (i, train_accuracy))
-                print('test accuracy %g' % accuracy.eval(feed_dict={
-                      x: data.test.images, y_: toOne(data.test.labels, flag), keep_prob: 1.0}))
+                print('test accuracy %g' % accuracy.eval(feed_dict={x: data.test.images, y_: toOne(data.test.labels, flag), keep_prob: 1.0}))
                 # print('test accuracy %g' % accuracy.eval(feed_dict={x: data.test.images, y_: data.test.labels, keep_prob: 1.0}))
         print(y_conv.eval(feed_dict={x: data.test.images, keep_prob: 1.0}))
         for test_x, test_y in zip(data.test.images, data.test.labels):
-            answer = sess.run(tf.argmax(y_conv, 1), feed_dict={
-                              x: [test_x], keep_prob: 1.0})
+            answer = sess.run(tf.argmax(y_conv, 1), feed_dict={x: [test_x], keep_prob: 1.0})
             test_value = test_y.argmax(0)
             test_value = 0 if flag == test_value else 1
             if test_value != answer[0]:
@@ -62,7 +56,7 @@ def main():
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     run_to_cal_accuracy(data, accuracy, train_step)
-    # run_to_estimate(data, accuracy)
+    run_to_estimate(data, accuracy, train_step)
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from methods_for_model import weight_variable, bias_variable, conv2d, max_pool_2x2
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
+x = tf.placeholder(tf.float32, shape=[None, 28 * 28])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 W_conv1 = weight_variable([5, 5, 1, 32])
@@ -12,16 +12,16 @@ x_image = tf.reshape(x, [-1, 28, 28, 1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5, 5, 32, 128])
+b_conv2 = bias_variable([128])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([7 * 7 * 128, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 128])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
